9aaecc11011c211c0697bd55addf744a7221a99a
Navyavankadara <84624138+Navyavankadara@users.noreply.github.com>
2022-09-02 11:41:28 +0530

    n=int(input()) m=list(map(int,input().split())) for i in range(n):     if m[i]%2!=0:         print(m[i],end=" ") for i in range(n):     if m[i]%2==0:         print(m[i],end=" ")

---
+++ array_of_add_and_even_elements.py
@@ -0,0 +1,8 @@
+n=int(input())
+m=list(map(int,input().split()))
+for i in range(n):
+    if m[i]%2!=0:
+        print(m[i],end=" ")
+for i in range(n):
+    if m[i]%2==0:
+        print(m[i],end=" ")
