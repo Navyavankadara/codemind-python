5c457cdd2e1f25e7fc0ce4120c1becef53c4d4a9
Navyavankadara <84624138+Navyavankadara@users.noreply.github.com>
2022-09-23 10:51:51 +0530

    n,m=map(int,input().split()) a1=list(map(int,input().split())) a2=list(map(int,input().split())) s1=set(a1) s2=set(a2) c=0 s=0 for i in s1:     if i not in s2:         c+=1 for i in s2:     if i not in s1:         s+=1 print(c+s)

---
+++ Count_elements_II.py
@@ -0,0 +1,14 @@
+n,m=map(int,input().split())
+a1=list(map(int,input().split()))
+a2=list(map(int,input().split()))
+s1=set(a1)
+s2=set(a2)
+c=0
+s=0
+for i in s1:
+    if i not in s2:
+        c+=1
+for i in s2:
+    if i not in s1:
+        s+=1
+print(c+s)
